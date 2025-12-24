f4703449a68f3620e2dde5d978f5412beecd4e2e
Binisha Khadka <khadkabinisha48@gmail.com>
2025-12-24 15:41:29 +0545

    feat:UI look attractive

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,4 +1,4 @@
-
+'use client';
 
 import React from 'react';
 
@@ -35,24 +35,24 @@ const testimonials = [
 
 export default function Testimonials() {
   return (
-    <section id="Testimonials" className="max-w-7xl mx-auto py-20 px-6 flex flex-col lg:flex-row gap-12 items-center">
-            <div className="lg:w-1/2 bg-brand-blue p-10 md:p-16 rounded-tr-[5rem] rounded-br-[5rem] rounded-tl-[5rem] text-white self-stretch flex flex-col justify-center">
-        <h2 className="text-3xl md:text-4xl font-bold mb-8 ">
+    <section id="Testimonials" className="max-w-full py-20 flex flex-col lg:flex-row items-center ioverflow-hidden">
+            <div className="lg:w-1/2 bg-brand-blue p-10 md:p-16 pl-6 md:pl-[60px] rounded-tr-[5rem] text-white self-stretch flex flex-col justify-center">
+        <h2 className="text-3xl md:text-5xl font-bold mb-8 leading-tight">
           Hear From Our Parents And Community!
         </h2>
         
-        <div className="space-y-6 text-white/90 leading-relaxed">
+        <div className="space-y-6 text-white/90 leading-relaxed text-lg">
           <p>
             Welcome To Our Testimonials Section, Where You Can Discover The Heartfelt Stories And Genuine Experiences Of Our Parents And Communities.
           </p>
-          <p className="poppins-font">
+          <p className="poppins-font border-l-4 border-white/20 pl-4">
             &ldquo;Where real voices reflect the trust, growth, and joyful learning that define BetterMontessori. Explore the inspiring journeys shared by families who have been part of our Montessori experience.&rdquo;
           </p>
         </div>
       </div>
 
    
-      <div className="lg:w-1/2 w-full space-y-8">
+      <div className="lg:w-1/2 w-full space-y-8 lg:px-12 py-10">
         {testimonials.map((t) => (
           <div 
             key={t.id} 
